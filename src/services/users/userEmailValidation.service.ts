64b3c6083583104efaf385d807431f467386207a
oswaldo <91742210+oswaldo-deco@users.noreply.github.com>
2022-07-18 07:22:51 -0300

    feat(src\services\partner): implemented services for the partners routers

--- src/services/users/userEmailValidation.service.ts
+++ src/services/users/userEmailValidation.service.ts
@@ -1,6 +1,7 @@
 import { AppDataSource } from "../../data-source";
 import { User } from "../../entities/user.entity";
 import { AppError } from "../../errors/appError";
+import { checkId } from "../../utils/checkId.utils";
 
 const userEmailValidateService = async (id: string) => {
   const userRepository = AppDataSource.getRepository(User);
